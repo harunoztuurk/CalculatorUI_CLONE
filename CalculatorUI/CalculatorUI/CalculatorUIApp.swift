5a4540e07b6d6a44127dc82363ff4d72bfcc62a4
harunoztuurk <85230159+harunoztuurk@users.noreply.github.com>
2022-10-26 19:03:05 +0300

    Add files via upload

---
+++ CalculatorUI/CalculatorUI/CalculatorUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  CalculatorUI_CLONE
+//
+//  Created by Harun Öztürk on 26.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
